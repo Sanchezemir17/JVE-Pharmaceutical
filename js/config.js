1a42b40bba8118ab533283211efa7100334096e8
Sanchezemir17 <159109503+Sanchezemir17@users.noreply.github.com>
2025-11-09 09:15:42 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_CfK1p44hr', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3melas8dlqtuspbmam3bjhpq40', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_J2SE5ioFw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1h07n3f35v3k3d8hs66qk0gkpo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
